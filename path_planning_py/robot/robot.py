0ea5a64da71cb849f6645fd56ca7349da253b34e
Baltasar <baltasar.ls@gmail.com>
2022-05-04 21:18:41 +0200

    Basic robot implementation

--- path_planning_py/robot/robot.py
+++ path_planning_py/robot/robot.py
@@ -1,7 +1,64 @@
+from xmlrpc.client import Boolean
+from ..geometry import utils
+
 class Robot:
-    def __init__(self, params : dict) -> None:
-        self.config = params
-        self.status = {}
-    
-    def hello(self):
-        return "hello"
+    def __init__(self, params = {}) -> None:
+        self.position = params.get("position", [0,0,0])
+        self.rotMatrix = params.get(
+            "rotation",
+            [[1,0,0],[0,1,0],[0,0,1]])
+        self.stepSize = params.get("step", 10)
+        self.zoneDeviation = params.get("deviation", 1)
+        self.ideal_path = []
+
+    def addWaypoint(self, waypoint : list) -> None:
+        self.ideal_path.append(waypoint)
+
+    def reorient(self, nextWaypoint : int) -> None:
+        direction = utils.normalize(
+            self.ideal_path[nextWaypoint], self.position)
+        if self.rotMatrix[0] != direction:
+            self.rotMatrix = utils.getRotMatrix(direction)
+            print("Reoriented with direction {}".format(self.rotMatrix[0]))
+
+    def move(self) -> bool:
+        if len(self.ideal_path) == 0:
+            print("Undefined path")
+            return True
+        nextWaypoint = 0
+        stepNum = 0
+        #stepToComplete = self.stepSize
+        while nextWaypoint < len(self.ideal_path):
+            waypointDistance = utils.pointDistance(self.position, self.ideal_path[nextWaypoint])
+            while waypointDistance <= self.zoneDeviation:
+                print("Waypoint {} reached in step {}".format(nextWaypoint,stepNum))
+                nextWaypoint += 1
+                if nextWaypoint == len(self.ideal_path):
+                    print("End reached in step {}".format(stepNum))
+                    return True
+                waypointDistance = utils.pointDistance(self.position, self.ideal_path[nextWaypoint])
+            self.reorient(nextWaypoint)
+            stepToComplete = self.stepSize
+            while (waypointDistance - self.zoneDeviation) <= stepToComplete:
+                self.position = utils.movePointTowards(
+                        self.position,
+                        self.ideal_path[nextWaypoint],
+                        (waypointDistance - self.zoneDeviation)
+                    )
+                stepToComplete -= (waypointDistance - self.zoneDeviation)
+                print("Waypoint {} reached in step {}".format(nextWaypoint,stepNum))
+                nextWaypoint += 1
+                if nextWaypoint == len(self.ideal_path):
+                    print("End reached in step {}".format(stepNum))
+                    return True
+                self.reorient(nextWaypoint)
+                waypointDistance = utils.pointDistance(self.position, self.ideal_path[nextWaypoint])
+            self.position = utils.movePointTowards(
+                    self.position,
+                    self.ideal_path[nextWaypoint],
+                    stepToComplete
+                )
+            print("Step {} completed".format(stepNum))
+            stepNum += 1
+        print("Error: move completed without finding an end")
+        return False
--- test/test_robot.py
+++ test/test_robot.py
@@ -6,10 +6,25 @@ class DistanceTest(unittest.TestCase):
     def setUp(self):
         self.robot = robot.Robot({})
 
-    def test_hello(self):
+    def test_move_1(self):
         self.assertEqual(
-            self.robot.hello(),
-            "hello",
+            self.robot.move(),
+            True,
+            )
+    def test_move_2(self):
+        self.robot.addWaypoint([5,0,0])
+        self.robot.addWaypoint([17,0,0])
+        self.assertEqual(
+            self.robot.move(),
+            True,
+            )
+    def test_move_2(self):
+        self.robot.addWaypoint([30,0,0])
+        self.robot.addWaypoint([30,2,2])
+        self.robot.addWaypoint([30,2,20])
+        self.assertEqual(
+            self.robot.move(),
+            True,
             )
 
 
